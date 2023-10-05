64809671ad180feb58a4eb49891cfd8f0491ac6a
Hernan <h3rnangamarr@gmail.com>
2023-10-04 21:29:22 -0500

    feat: Spring Boot Project Creation

---
+++ src/main/java/net/main/RealidadAumentadaApplication.java
@@ -0,0 +1,13 @@
+package net.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealidadAumentadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealidadAumentadaApplication.class, args);
+	}
+
+}
